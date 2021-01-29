d8542c1dd82a25d61c9284193f55b3ef7ad8e01c
Ravindra Dahal <dahalravindra@gmail.com>
2021-01-29 23:53:12 +0545

    Made API requests to the Unsplash API and printed out the results :boom:

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,27 @@
 import React from 'react';
-import SearchBar from './SearchBar'
+import axios from 'axios';
+import SearchBar from './SearchBar';
 
 class App extends React.Component {
-	onSearchSubmit(term){
-		console.log(term)
-	}
+  async onSearchSubmit(term) {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization:
+          'Client-ID e3O1JZrcRx1KOHWhK0uPMGIBZbPHd1OdryXnFk1q3uE',
+      },
+    }
+    );
+    console.log(response.data.results)
+  }
 
-	render(){
-	return (
-		<div className='ui container' style={{marginTop:'10px'}}>
-			<SearchBar onSubmit={this.onSearchSubmit} />
-		</div>
-	)
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: '10px' }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
 }
-};
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,22 +3,22 @@ import React from 'react';
 class SearchBar extends React.Component {
   state = { term: '' };
 
-  onFormSubmit = (event) => {
+  onFormSubmit = event => {
     event.preventDefault();
 
-    this.props.onSubmit(this.state.term)
-  }
+    this.props.onSubmit(this.state.term);
+  };
 
   render() {
     return (
       <div className="ui segment">
-        <form onSubmit={ this.onFormSubmit } className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input 
-              type="text" 
+            <input
+              type="text"
               value={this.state.term}
-              onChange={(e) => this.setState({ term: e.target.value })} 
+              onChange={e => this.setState({ term: e.target.value })}
             />
           </div>
         </form>
