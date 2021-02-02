0651ca5179605febace66dccae222c34d1dab976
Ravindra Dahal <dahalravindra@gmail.com>
2021-02-02 21:27:05 +0545

    Updated styles :lipstick:

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import unsplash from '../api/unsplash'
-import ImageList from './imageList'
+import ImageList from './ImageList'
 
 class App extends React.Component {
   state = { images: [] };
--- src/components/imageList.js
+++ src/components/imageList.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './ImageList.css'
 
 const ImageList = (props) => {
 	const images = props.images.map(({description,id,urls}) => {
 		return <img alt={description} key={id} src={urls.small} /> 
 	});
-	return <div>{images}</div>;
+	return <div className='image-list'>{images}</div>;
 };
 
 export default ImageList;
